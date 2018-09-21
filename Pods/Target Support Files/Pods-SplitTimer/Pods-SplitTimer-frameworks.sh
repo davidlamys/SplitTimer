a80347ccee03d87d841cbc2e9447bc1a42cb67ee
David Lam <davidlys@gmail.com>
2018-09-22 04:14:56 +0800

    Include RxSwift and RxCocoa for testing

--- Pods/Target Support Files/Pods-SplitTimer/Pods-SplitTimer-frameworks.sh
+++ Pods/Target Support Files/Pods-SplitTimer/Pods-SplitTimer-frameworks.sh
@@ -143,11 +143,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa-framework/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift-framework/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa-framework/RxCocoa.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxSwift-framework/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
